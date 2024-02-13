61441f59fd100c5fd29800c8d322881b43912320
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2024-02-13 18:06:50 +0200

    required_del

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -7,9 +7,9 @@ const createContactSchema = Joi.object({
 });
 
 const updateContactSchema = Joi.object({
-  name: Joi.string().min(2).max(255).required(),
-  email: Joi.string().min(4).max(255).required().email(),
-  phone: Joi.string().min(4).max(20).required().pattern(/\+?[0-9\s\-\(\)]+/),
+  name: Joi.string().min(2).max(255),
+  email: Joi.string().min(4).max(255).email(),
+  phone: Joi.string().min(4).max(20).pattern(/\+?[0-9\s\-\(\)]+/),
 });
 
 module.exports = { createContactSchema, updateContactSchema };
