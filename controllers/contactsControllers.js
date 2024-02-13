e7e354d0824698d0571cedb3cc49b04bbf6090a2
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2024-02-13 10:56:16 +0200

    change

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -62,33 +62,27 @@ const createContact = async (req, res) => {
   }
 };
 
-
 const updateContact = async (req, res) => {
   try {
-    // Отримуємо id з параметрів запиту
     const { id } = req.params;
-    // Отримуємо дані з тіла запиту
     const { name, email, phone } = req.body;
 
-    // Перевірка, чи передано хоча б одне поле в тілі запиту
     if (!name || !email || !phone) {
-      return res.status(400).json({ message: "Body must have at least one field" });
+      return res
+        .status(400)
+        .json({ message: "Body must have at least one field" });
     }
-    // Оновлення контакту за допомогою функції updateContact
+
     const result = await updateContacts(id, { name, email, phone });
 
     if (result.status === 200) {
-      // Якщо контакт оновлено успішно, повертаємо оновлені дані
       return res.status(200).json(result.data);
     } else if (result.status === 404) {
-      // Якщо контакт не знайдено, повертаємо відповідь зі статусом 404
       return res.status(404).json({ message: "Not found" });
     } else {
-      // Інакше повертаємо відповідь зі статусом 500
       return res.status(500).json({ message: "Internal server error" });
     }
   } catch (error) {
-    // Якщо сталася помилка, повертаємо відповідь зі статусом 500
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
   }
