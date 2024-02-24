f7e825a3686383517f953cac3d6c73ef36726637
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2024-02-24 09:20:56 +0200

    populate

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -3,14 +3,20 @@ const Contact = require("../model/contacts");
 
 const getAllContacts = async (req, res) => {
   const { _id } = req.user;
+
   const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
+
   const filterValue = favorite ? { owner: _id, favorite } : { owner: _id };
-  const contacts = await Contact.find( filterValue ).skip(skip).limit(limit);
+
+  const contacts = await Contact.find(filterValue, "", {
+    skip,
+    limit,
+  }).populate("owner", "email subscription");
     res.status(200).json(contacts);
 
   };
-  
+
 
 
 const getOneContact = async (req, res) => {
